8af4c95300f1b63789aa4eade8330ce8a7a2b5a2
Martekode <BrianDesmedt9000@hotmail.com>
2022-09-17 20:10:19 +0200

    database connection established

--- server/index.js
+++ server/index.js
@@ -1,5 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import cors from 'cors';
+import mysql from 'mysql'
 
+const app = express();
+const Mysql = mysql;
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+
+app.use(cors());
+
+
+// later to be stored in env variables
+const PORT = process.env.PORT || 5000;
+
+
+// connection to the database
+const con = Mysql.createConnection({
+    host: "localhost",
+    user : "becode",
+    password : "Becode6-05"
+});
+
+// throzs error when connection error occurs
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("connected");
+})
