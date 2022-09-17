01a62bf87eb9bf9598988abea6aad749d7348992
Martekode <BrianDesmedt9000@hotmail.com>
2022-09-18 01:19:41 +0200

    added book controller for query logic

---
+++ server/controllers/books.js
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from 'uuid';
+
+let books = [];
+
+export const getBooks = (res , req) => {
+    //logic
+};
+
+export const getBook = (res , req) => {
+    //logic
+};
+
+export const createBook = (res , req) => {
+    //logic
+};
+
+export const deleteBook = (res , req) => {
+    //logic
+};
+
+export const updateBook = (res , req) => {
+    //logic
+};
