6441fd1cf15c2e848471c5f332b674de7823d0b3
Martekode <BrianDesmedt9000@hotmail.com>
2022-09-20 16:37:02 +0200

    async transorfmation to the function

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -1,7 +1,10 @@
 import BookServiceInstance from "../services/BookService.js";
 
-export const getBooks = (res , req) => {
-    //logic 
+export const getBooks = async (req, res) => {
+    //logic
+    // we await the result of the fire method and send it back to the client.
+    let books = await BookServiceInstance.fireAllBooksQuery();
+    res.send(books);
 };
 
 export const getBook = (res , req) => {
