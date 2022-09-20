af381550d008c625dc4a96450329e83c151b5225
Martekode <BrianDesmedt9000@hotmail.com>
2022-09-20 15:06:36 +0200

    db connection copied to bookservice

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import mysql from 'mysql';
 import booksRoutes from './routes/books.js';
+import BookServiceInstance from './services/bookService.js';
 
 const app = express();
-const Mysql = mysql;
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -19,20 +18,7 @@ app.use('/api/books', booksRoutes);
 // later to be stored in env variables
 const PORT = process.env.PORT || 8080;
 
-
-// connection to the database
-const con = Mysql.createConnection({
-    host: "localhost",
-    user : "becode",
-    password : "Becode6-05"
-});
-
-// throzs error when connection error occurs
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("connected to the database");
-})
-
+BookServiceInstance.connect();
 
 app.listen(PORT , ()=> console.log("listening on port " + PORT));
 
